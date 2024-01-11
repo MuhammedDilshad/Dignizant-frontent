2af8d81b53aae0cc848c533eeae5bb954bc193f5
dilshad <md.mdsar@gmail.com>
2024-01-11 17:10:44 +0530

    Api updated

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 
+// export const Axios = axios.create({
+//   baseURL: "http://localhost:3400/",
+// });
+// export const AxiosAdmin = axios.create({
+//   baseURL: "http://localhost:3400/admin/",
+// });
 export const Axios = axios.create({
-  baseURL: "http://localhost:3400/",
+  baseURL: "https://dignizant-backend.onrender.com/",
 });
+
 export const AxiosAdmin = axios.create({
-  baseURL: "http://localhost:3400/admin/",
+  baseURL: "https://dignizant-backend.onrender.com/admin/",
 });
